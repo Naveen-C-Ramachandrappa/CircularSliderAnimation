861049f18e95493be8966bdef74f19a4f11419f4
NaveenRam <accessnaveen@gmail.com>
2024-10-16 10:01:51 -0500

    Initial Commit

---
+++ CricularSlider/CricularSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricularSliderApp.swift
+//  CricularSlider
+//
+//  Created by Naveen Ramachandrappa on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CricularSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
